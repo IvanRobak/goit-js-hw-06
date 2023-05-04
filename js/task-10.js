49a672736b66fdfd675536176ed77f0c156ee703
ivan1810 <78267912+ivan1810@users.noreply.github.com>
2023-05-04 12:10:56 +0300

    some

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(input.value);
+  let size = 30;
+  let boxesArr = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesArr.push(box);
+    size += 10;
+  }
+
+  boxes.append(...boxesArr);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
